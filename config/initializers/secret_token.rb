6dfce9aade24d814a924f4d8f68885168e0c5030
T Riboulet <riboulet@gmail.com>
2010-12-17 10:45:02 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Poulpzor::Application.config.secret_token = 'c8003f7602ac6d78b807028c028918d5e3a75c90d58916c86d916a8afb769ac478efc8dc262e1ce2e6fe4eab1f19ea6cf1c0b3458547d50be72186afecd5cf0d'
