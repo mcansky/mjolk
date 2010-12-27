4e60633d18dc0cf6082186711c2a9476474de719
T Riboulet <riboulet@gmail.com>
2010-12-27 14:08:24 +0100

    using devise process, cleaning up

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -14,9 +14,4 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     end
   end
 
-  def create
-    user = User.new(:name => params[:name], :email => params[:email], :password => params[:password], :password_confirmation => params[:password_confirmation])
-    user.save
-    redirect_to :controller => "posts"
-  end
 end
