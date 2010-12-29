4dda298e407eba00b57734a2f074e61efeb4a26e
T Riboulet <riboulet@gmail.com>
2010-12-29 21:15:54 +0100

    only what's needed

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -4,13 +4,14 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     logger.info(env["omniauth.auth"].inspect)
     @user = User.find_for_twitter_oauth(env["omniauth.auth"], current_user)
     if !@user
-      session["devise.twitter_data"] = env["omniauth.auth"]
+      session["username"] = env["omniauth.auth"]['extra']['user_hash']['name']
       redirect_to new_user_registration_url
     elsif @user.persisted?
       flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => "Twitter"
       sign_in_and_redirect @user, :event => :authentication
     else
       session["devise.twitter_data"] = env["omniauth.auth"]
+      session["username"] = env["omniauth.auth"]['extra']['user_hash']['name']
       redirect_to new_user_registration_url
     end
   end
