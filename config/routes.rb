f7ed6e68fbd886ecca31d1135dd85bec0c10dac2
T Riboulet <riboulet@gmail.com>
2011-01-26 14:15:23 +0100

    fixing routes

--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,7 @@ Mjolk::Application.routes.draw do
   match 'posts/import_url', :to => 'posts#import_url'
   match 'posts/import_file', :to => 'posts#import_file'
   resources :bookmarks, :controller => "posts"
+  resources :tags
 
   # Sample of named route:
   #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
@@ -25,47 +26,6 @@ Mjolk::Application.routes.draw do
   #   resources :products
   resources :posts
 
-  # Sample resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Sample resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Sample resource route with more complex sub-resources
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', :on => :collection
-  #     end
-  #   end
-
-  # Sample resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
-
-  # You can have the root of your site routed with "root"
-  # just remember to delete public/index.html.
-  # root :to => "welcome#index"
   root :to => "application#index"
 
-  # See how all your routes lay out with "rake routes"
-
-  # This is a legacy wild controller route that's not recommended for RESTful applications.
-  # Note: This route will make all actions in every controller accessible via GET requests.
-  # match ':controller(/:action(/:id(.:format)))'
 end
