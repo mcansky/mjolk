9fea1420ab7422cbcae8f1c247c344d01a15c019
T Riboulet <riboulet@gmail.com>
2011-01-28 13:06:19 +0100

    remove auth check on app controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,5 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  authorize_resource
 
 #  rescue_from CanCan::AccessDenied do |exception|
 #    flash[:alert] = exception.message
